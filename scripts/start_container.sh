6a6c5acd84d8ac61c12a4caeb0bd3771d9d3fabc
Ashutosh Bambal <92073828+ashubambal@users.noreply.github.com>
2023-08-19 23:56:23 +0530

    v10

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
 # Pulling the docker container from DockerHub
-#docker pull softconsist/django-notes-app
+docker pull softconsist/django-notes-app
 
 # Run the docker image as a container
-#docker run -d -p 8000:8000 softconsist/django-notes-app
-
-# Generate a timestamp in the format YYYY-MM-DD-HHMMSS
-timestamp=$(date +%Y%m%d%H)
-
-# Define the Docker image name and tag
-docker_image="softconsist/django-notes-app"
-docker_tag="$docker_image:$timestamp"
-
-# Pulling the docker container from DockerHub
-docker pull "$docker_image"
-
-# Run the docker image as a container
-docker run -d -p 8000:8000 "$docker_tag"
+docker run -d -p 8000:8000 softconsist/django-notes-app
