cede02c700124339ea22dc3f7ac4fcb4e53e3e64
Ashutosh Bambal <92073828+ashubambal@users.noreply.github.com>
2023-08-20 00:26:08 +0530

    v12

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Pulling the docker container from DockerHub
-docker pull softconsist/django-notes-app
+docker pull softconsist/django-notes-app:latest
 
 # Run the docker image as a container
-docker run -d -p 8000:8000 softconsist/django-notes-app
+docker run -d -p 8000:8000 softconsist/django-notes-app:latest
